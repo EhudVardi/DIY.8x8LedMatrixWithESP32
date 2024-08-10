14a6b1ede76dbd5a323d9a918371cfde6d5cef52
mister stam <stam@stam.com>
2024-08-10 14:03:03 +0300

    implement the animation class to init the game and step it

--- Sources/ArduinoIDE/16x16LedMatrixWithESP32_Animations/SnakeInteractiveAnime.cpp
+++ Sources/ArduinoIDE/16x16LedMatrixWithESP32_Animations/SnakeInteractiveAnime.cpp
@@ -1,12 +1,26 @@
 #include "SnakeInteractiveAnime.h"
 
 SnakeInteractiveAnime::SnakeInteractiveAnime(int size)
-    : BaseInteractiveAnime(size, 2) {}
+    : BaseInteractiveAnime(size, 2), game(size,size) {}
 
 void SnakeInteractiveAnime::init() {
-
+	game.InitGame();
 }
 
 void SnakeInteractiveAnime::step() {
-	
+	// step the game
+	game.StepGame();
+	// update display matrix from game board
+	clearMatrix();
+  setPixel(0, 0, true);
+  setPixel(15, 0, true);
+	Matrix2D gameBoard = game.GetBoard();
+	for (int i=0; i<gameBoard.GetWidth(); i++) {
+		for (int j=0; j<gameBoard.GetHeight(); j++) {
+			if (gameBoard.IsBodyCell(i,j))
+				setPixel(i, j, true);
+		}
+	}
 }
+
+void SnakeInteractiveAnime::setInput(int inputIdx) { }
