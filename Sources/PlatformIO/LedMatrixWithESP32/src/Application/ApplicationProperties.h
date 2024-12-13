3b220e88bbe568364adc5423b3cbe7bcca365304
mister stam <stam@stam.com>
2024-12-13 11:18:03 +0200

    version 1.0.1

--- Sources/PlatformIO/LedMatrixWithESP32/src/Application/ApplicationProperties.h
+++ Sources/PlatformIO/LedMatrixWithESP32/src/Application/ApplicationProperties.h
@@ -4,8 +4,8 @@
 #include <Arduino.h>
 
 const String APP_NAME_TEXT = "LED MATRIX DISPLAY";
-const String APP_VERSION_TEXT = "1.0.0";
+const String APP_VERSION_TEXT = "1.0.1";
 
-const std::string APP_TITLE_STRING = std::string(APP_NAME_TEXT.c_str()) + ". VERSION" + std::string(APP_VERSION_TEXT.c_str()); 
+const std::string APP_TITLE_STRING = std::string(APP_NAME_TEXT.c_str()) + "  VER." + std::string(APP_VERSION_TEXT.c_str()); 
 
 #endif // APPLICATION_PROPERTIES_H
